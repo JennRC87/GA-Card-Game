ff03c920a39259fd3db4fb8be4ee5877c6ebd0dd
Jennifer Christie <babegrrl69@Jennifer-Christies-MacBook-Pro.local>
2016-10-15 15:17:12 -0400

    Fixed  if else statments in js

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 //console.log("JS file is connected to HTML! Woo!")
 
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+var cardOne = 'queen';
+var cardTwo = 'king';
+var cardThree = 'queen';
+var cardFour = 'king';
 
-if (cardOne = cardTwo) {
-	alert = ("You did it! Yay!");
+if (cardOne === cardThree) {
+	alert ('You did it! Yay!');
 } else {
-	alert = ("Smh. &#9786 Try Again.")
+	alert ('Smh. &#9786 Try Again.');
+}
